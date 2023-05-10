d4b5428e466358931a058b09346ff4d952bcb7ef
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2023-05-10 17:53:05 +0200

    add panda, fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Typography } from "@/components/Typography/Typography";
+import { Typography } from "../components/typography/Typography";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Typography variant="h1">Hello Main Page</Typography>
+      <Image src='/panda.svg' alt="panda" width={400} height={400} />
     </div>
   )
 }
